c6f6c7f713d2929459e9882fb4436de9eb5fb0f1
42kharuya <haruya.0411.k@gmail.com>
2024-12-19 15:56:09 +0900

    Passed ft_printf

--- main.c
+++ main.c
@@ -1,50 +1,36 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: kharuya <kharuya@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/08 03:20:28 by kharuya           #+#    #+#             */
-/*   Updated: 2024/12/08 01:09:07 by kharuya          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "ft_printf.h"
 
 int main(void)
 {
-    int result;
+    // %c
+    printf("length:%d\n",ft_printf("%c %c %c",'h', 'H', 48));
+    printf("\nlength:%d\n",printf("%c %c %c",'h', 'H', 48));
+    // %s
+    printf("length:%d\n",ft_printf("%s %s %s","Hello", "Good Morning", "Good bye"));
+    printf("\nlength:%d\n",printf("%s %s %s","Hello", "Good Morning", "Good bye"));
+    // %p
+    printf("length:%d\n",ft_printf("%p %p %p","Hello", "Good Morning", "Good bye"));
+    printf("\nlength:%d\n",printf("%p %p %p","Hello", "Good Morning", "Good bye"));
+    // %d
+    printf("length:%d\n",ft_printf("%d %d %d",INT_MAX, INT_MIN, 0));
+    printf("\nlength:%d\n",printf("%d %d %d",INT_MAX, INT_MIN, 0));
+    // %i
+    printf("length:%d\n",ft_printf("%i %i %i",INT_MAX, INT_MIN, 0));
+    printf("\nlength:%d\n",printf("%i %i %i",INT_MAX, INT_MIN, 0));
+    // %u
+    printf("length:%d\n",ft_printf("%u %u ",UINT_MAX, 0));
+    printf("\nlength:%d\n",printf("%u %u ",UINT_MAX, 0));
+    // %x
+    printf("\nlength:%d\n",ft_printf("%x %x %x", "Hello", "Good Morning", "Good bye"));
+    printf("\nlength:%d\n",printf("%x %x %x", "Hello", "Good Morning", "Good bye"));
+    // %X
+    printf("\nlength:%d\n",ft_printf("%X %X %X", "Hello", "Good Morning", "Good bye"));
+    printf("\nlength:%d\n",printf("%X %X %X", "Hello", "Good Morning", "Good bye"));
+    // %%
+    printf("\nlength:%d\n",ft_printf("%%"));
+    printf("\nlength:%d\n",printf("%%"));
+    //mix
+    printf("\nlength:%d\n",ft_printf("%c %s %p %d %i %u %x %X %%", 'H', "Good Morning", "Good bye", INT_MAX, INT_MIN, UINT_MAX, "Thank you", "Thank you"));
+    printf("\nlength:%d\n",printf("%c %s %p %d %i %u %x %X %%", 'H', "Good Morning", "Good bye", INT_MAX, INT_MIN, UINT_MAX, "Thank you", "Thank you"));
 
-    result = 0;
-    // テスト1: 文字出力
-    result = ft_printf("Character test: %c\n", 'A');
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト2: 文字列出力
-    result = ft_printf("String test: %s\n", "Hello, World!");
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト3: ポインタ出力
-    int num = 42;
-    result = ft_printf("Pointer test: %p\n", &num);
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト4: 整数出力
-    result = ft_printf("Integer test: %d, Negative test: %i\n", 123, -456);
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト5: 符号なし整数出力
-    result = ft_printf("Unsigned test: %u\n", 123456789U);
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト6: 16進数出力（小文字と大文字）
-    result = ft_printf("Hex test (lowercase): %x, Hex test (uppercase): %X\n", 255, 255);
-    printf("Printed characters: %d\n\n", result);
-
-    // テスト7: パーセント文字の出力
-    result = ft_printf("Percent test: %%\n");
-    printf("Printed characters: %d\n\n", result);
-
-    return 0;
 }
